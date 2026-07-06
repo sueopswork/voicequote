1933cac763902ce2ec1afbdb7148ba3982436c47
Suad Mussa <sue.ops.work@gmail.com>
2026-07-06 15:17:09 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-function App() {
-return (
-<div className="p-6 text-center">
-<h1 className="text-2xl font-bold">VoiceQuote Loading...</h1>
-<p className="mt-2 text-gray-600">
-UI upgrade coming next push 🚀
-</p>
-</div>
+ReactDOM.createRoot(document.getElementById("root")).render(
+<React.StrictMode>
+<App />
+</React.StrictMode>
 );
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
